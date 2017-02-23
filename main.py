557d4df8f325674e7ac3b25d656dc2490e0ee614
Alexander Cochrane <acochrane@almightycorp.com>
2017-02-23 10:08:12 -0400

    changed imports, fixed annoying things, and commented out the entire thing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 __author__ = 'Ben Covey, Brandon Christen, Alex Cochrane'
-from PIL import Image
+#from Pillow import Image
+import matplotlib.pyplot as plt
 from numpy import array
-img = Image.open("input.png")
+from skimage import data
+"""img = Image.open("input.png")
 rgb_im = img.convert('RGB')
 pixel_array = []
 # if(r == 181 or g == 230 or b == 29):
@@ -14,15 +16,13 @@ try:
             r, g, b = rgb_im.getpixel((x*8, y*8))
             # I am using that maybe RGB values to allow lightning tolerances. I was thinking of making those into a
             # single array, then comparing that array to the rbg values. Or Switch statement
-            if(r == 175 or r == 176 or r == 177 or r == 178 or r == 179 or r == 180 or r == 181 or r == 182 or r == 183 or r == 184 or r == 185):
-                if(g == 225 or g == 226 or g == 227 or g == 228 or g == 229 or g == 230 or g == 231 or g == 232 or g == 233 or g == 234 or g == 234):
-                    if(b == 24 or b == 25 or b == 26 or b == 27 or b == 28 or b == 29 or b == 30 or b == 31 or b == 32 or b == 33 or b == 34):
+            if(r in range(175, 186)):
+                if(g in range(225, 235)):
+                    if(b in range(24, 35)):
                         pixel_array.append('%d,%d' % (x*8, y*8))
                         print('%d,%d' % (x*8, y*8))
                         print(r, g, b)
 finally:
     print(len(pixel_array))
     print(pixel_array)
-    print("End of range")
-
-
+    print("End of range")"""
